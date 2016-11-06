0f2f1dc8055fcbf4e722c4e10ebebb8de327387c
Joan <geneijoan@hotmail.com>
2016-11-06 20:18:59 +0100

    PUJADA DES DE RAMON

--- frmMissatges.vb
+++ frmMissatges.vb
@@ -14,4 +14,21 @@ Public Class frmMissatges
         Me.Close()
     End Sub
 
+    Private Sub txtMissatges_DoubleClick(sender As Object, e As EventArgs) Handles txtMissatges.DoubleClick
+        Dim nroFeinaC As String = ""
+        Dim auxC As String
+
+        If Mid(txtMissatges.SelectedText, 1, 1) = "#" Then
+            For i = 2 To txtMissatges.SelectedText.Length
+                auxC = Mid(txtMissatges.SelectedText, i, 1)
+                If auxC < "0" Or auxC > "9" Then Exit For
+                nroFeinaC &= auxC
+            Next
+            If nroFeinaC.Length > 0 Then
+                frmPrincipal.MostrarFeina(CLng(nroFeinaC))
+            End If
+            txtMissatges.SelectionLength = 0
+        End If
+    End Sub
+
 End Class
